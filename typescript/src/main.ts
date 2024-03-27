e157f3382b345890158537ec6f4ae6a50a5af515
HaroldKM <harodasus1230@gmail.com>
2024-03-27 15:50:22 +0100

    type topics challenge

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -2,8 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './tutorial.ts'
-
+import './type challenges/challenge-1.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
---
+++ typescript/src/type challenges/challenge-1.ts
@@ -0,0 +1,17 @@
+// Create a variable of type string and try to invoke a string method on it.
+
+let yourName: string = "John"
+console.log(yourName.toUpperCase())
+console.log(yourName);
+
+
+// Create a variable of type number and try to perform a - mathematical operation on it.
+let math: number = 20
+math = math++
+console.log(math);
+
+// Create a variable of type boolean and try to perform a logical operation on it.
+let isColored = true
+isColored = 1+1 ? true : false
+console.log(isColored);
+
