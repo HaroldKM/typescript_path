77218f77df63978dd344296951b24bf3042b78bb
HaroldKM <harodasus1230@gmail.com>
2024-03-29 09:24:37 +0100

    array-challenge-3

---
+++ typescript/src/challenges/array-challenge-3.ts
@@ -0,0 +1,8 @@
+// Create an array temperatures of type number[] and assign it some values. Then, try to add a string value to it.
+let temperatures: number[] = [19, 20, 30, -21]
+
+// Create an array colors of type string[] and assign it some values. Then, try to add a boolean value to it.
+let colors: string[] =  ["red", "green", "blue"]
+
+// Create an array mixedArray of type (number | string)[] and assign it some values. Then, try to add a boolean value to it.
+let mixedArray: (number | string)[] = ["hello", 1, "sorry", 2, "congratulation", 3]
